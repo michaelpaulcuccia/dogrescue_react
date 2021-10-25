3063e6646936db7e36b4efe774ef5176f0cb8745
Michael Cuccia <michael.cuccia@egplusww.com>
2021-10-25 13:58:56 -0400

    continue search

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -3,10 +3,16 @@ import styled from "styled-components";
 import * as palette from '../constants/palette';
 
 const Container = styled.div`
-  height: 175px;
   width: 100%;
   display: flex;
-  justify-content: space-evenly;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const QuestionContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
 `;
 
 const GenderContainer = styled.div`
@@ -19,6 +25,7 @@ const GenderContainer = styled.div`
   h3 {
     font-family: "Gochi Hand", cursive;
     font-size: 28px;
+    padding-bottom: 10px;
     color: ${palette.PURPLE};
   }
 
@@ -39,6 +46,7 @@ const GenderContainer = styled.div`
   h2 {
     font-family: "Gochi Hand", cursive;
     font-size: 28px;
+    padding-top: 10px;
     color: ${palette.PURPLE};
   }
 `;
@@ -52,6 +60,7 @@ const BreedContainer = styled.div`
   h3 {
     font-family: "Gochi Hand", cursive;
     font-size: 28px;
+    padding-bottom: 10px;
     color: ${palette.PURPLE};
   }
 
@@ -70,6 +79,20 @@ const BreedContainer = styled.div`
   }
 
   h2 {
+    font-family: "Gochi Hand", cursive;
+    font-size: 28px;
+    padding-top: 10px;
+    color: ${palette.PURPLE};
+  }
+`;
+
+const ResultsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 15px;
+
+  h2, p {
     font-family: "Gochi Hand", cursive;
     font-size: 28px;
     color: ${palette.PURPLE};
@@ -84,12 +107,18 @@ const Search = props => {
   const [showGenderForm, setShowGenderForm] = useState(true);
   const [showSelectBreed, setShowSelectBreed] = useState(false)
   const [breed, setBreed] = useState("");
+  const [showBreedForm, setShowBreedForm] = useState(true);
   const [showBreedChoice, setShowBreedChoice] = useState(false)
 
   useEffect(()=> {
     setDogs(props.dogs);
   },[props]);
-  
+
+  let boyDogs = dogs.filter(dog => dog.sex === 'male');
+  let girlDogs = dogs.filter(dog => dog.sex === 'female');
+
+  //TODO: create a breed filter function using gender
+
   const handleGenderSubmit = (event) => {
     event.preventDefault();
     setShowGenderSelection(true);
@@ -99,50 +128,63 @@ const Search = props => {
 
   const handleBreedSubmit = (event) => {
     event.preventDefault();
+    setShowBreedForm(false);
     setShowBreedChoice(true);
   }
 
   return (
     <Container>
 
-      <GenderContainer>
-        {
-          showGenderForm &&
-          <>
-          <h3>Good Boy? Or, Good Girl?</h3>
-          <form onSubmit={handleGenderSubmit}>
-            <select onChange={(e) => setGender(e.target.value)}>
-              <option disabled selected>
-                Select your option
-              </option>
-              <option value="boy">Boy</option>
-              <option value="girl">Girl</option>
-            </select>
-            <button type="submit">Submit</button>
-          </form>
-          </>
-        }
-      {showGenderSelection && <h2>You have chosen a {gender}!</h2>}
-      </GenderContainer>
+      <QuestionContainer>
+        <GenderContainer>
+          {
+            showGenderForm &&
+            <>
+            <h3>Good Boy? Or, Good Girl?</h3>
+            <form onSubmit={handleGenderSubmit}>
+              <select onChange={(e) => setGender(e.target.value)}>
+                <option disabled selected>
+                  Select your option
+                </option>
+                <option value="boy">Boy</option>
+                <option value="girl">Girl</option>
+              </select>
+              <button type="submit">Submit</button>
+            </form>
+            </>
+          }
+        </GenderContainer>
 
-      {
-        showSelectBreed &&
-        <BreedContainer>
-         <h3>Choose a Breed</h3>
-        <form onSubmit={handleBreedSubmit}>
-          <select onChange={(e) => setBreed(e.target.value)}>
-            <option disabled selected>
-              Select a Breed
-            </option>
-            {dogs.map((item, i)=> (
-              <option key={i} value={item.breed}>{item.breed}</option>
-            ))}
-          </select>
-          <button type="submit">Submit</button>
-        </form>
+        {
+          showSelectBreed &&
+          <BreedContainer>
+          {
+            showBreedForm &&
+            <>
+              <h3>Choose a Breed</h3>
+                <form onSubmit={handleBreedSubmit}>
+                  <select onChange={(e) => setBreed(e.target.value)}>
+                    <option disabled selected>
+                      Select a Breed
+                    </option>
+                      {dogs.map((item, i)=> (
+                        <option key={i} value={item.breed}>{item.breed}</option>
+                      ))}
+                  </select>
+                  <button type="submit">Submit</button>
+                </form>
+            </>
+          }
+        
+          </BreedContainer>
+        }
+      </QuestionContainer>
+
+      <ResultsContainer>
+        {showGenderSelection && <h2>You have chosen a {gender}!</h2>}
+        {gender !== "" && gender === 'boy' && showGenderSelection ? <p>There are {boyDogs.length} boys</p> : gender !== "" && gender === 'girl' && showGenderSelection ? <p>There are {girlDogs.length} girls</p>  : "" }
         {showBreedChoice && <h2>You have chosen {breed}!</h2>}
-        </BreedContainer>
-      }
+      </ResultsContainer>
 
     </Container>
 
