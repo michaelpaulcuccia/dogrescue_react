f0ef74bb5fc287ec80db1baea0b1cd9a945982b6
Michael Cuccia <michael.cuccia@egplusww.com>
2021-11-16 10:18:03 -0500

    Success2 page, need button component

--- client/src/pages/SuccessDog.js
+++ client/src/pages/SuccessDog.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Success2 from '../components/Success2';
 
-const SuccessDog = () => {
+const SuccessDog = ({dogs}) => {
+
     return (
         <div>
             Hello, from SuccessDog.js
+            <Success2 dogs={dogs}/>
         </div>
     )
 }
